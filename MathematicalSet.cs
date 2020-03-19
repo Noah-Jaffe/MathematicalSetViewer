203ecf08c4f84d83490506d34f16b92d01cc15a3
nmj101 <noah0m0jaffe@gmail.com>
2020-03-19 18:14:50 -0400

    Adding mathematicalSet abstraction

---
+++ MathematicalSet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace MathematicalSetViewer
+{
+    abstract class MathematicalSet
+    {
+        /// <summary>
+        /// The active color pallet to use when rendering an image
+        /// </summary>
+        public Color[] ColorPallet { get; set; }
+
+        /// <summary>
+        /// Uses the Decimal form of the XY structure to hold the screen resolution
+        /// </summary>
+        public XY ScreenDimentions { get; set; }
+
+        /// <summary>
+        /// Calculates the given range scaled to the current screen dimentions
+        /// TODO: might change to scale to a given range of pixles instead
+        /// </summary>
+        /// <param name="lowerLeft">the lower left coordinates of the needed range to be calculated</param>
+        /// <param name="topRight">the top right coordinate of the needed range to be calculated</param>
+        /// <returns>TODO: figure out what format I am returning (if any?)</returns>
+        public abstract Object CalculateRange(XY lowerLeft, XY topRight);
+
+    }
+}
+
---
+++ XY.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MathematicalSetViewer
+{
+
+    /// <summary>
+    /// A struct to hold 2 numbers
+    /// TYPE    >Bytes=> Variable Names 
+    /// bytes   >   1 => b1, b2
+    /// short   >   2 => s1, s2
+    /// int     >   4 => i1, i2
+    /// double  >   8 => d1, d2
+    /// long    >   8 => l1, l2
+    /// Decimal >  16 => D1, D2
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct XY
+    {
+        [FieldOffset(0)]
+        public byte Xb;
+        [FieldOffset(sizeof(Decimal))]
+        public byte Yb;
+        [FieldOffset(0)]
+        public short Xs;
+        [FieldOffset(sizeof(Decimal))]
+        public short Ys;
+        [FieldOffset(0)]
+        public int Xi;
+        [FieldOffset(sizeof(Decimal))]
+        public int Yi;
+        [FieldOffset(0)]
+        public double Xd;
+        [FieldOffset(sizeof(Decimal))]
+        public double Yd;
+        [FieldOffset(0)]
+        public long Xl;
+        [FieldOffset(sizeof(Decimal))]
+        public long Yl;
+        [FieldOffset(0)]
+        public Decimal X;
+        [FieldOffset(sizeof(Decimal))]
+        public Decimal Y;
+    }
+}
